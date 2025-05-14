93b053e7cb3f7447c554a2d3e7edb79a6e48f031
Boda Raj Tharan <rajtharan1125@gmail.com>
2025-05-14 12:32:26 +0530

    Add files via upload

---
+++ rajtharan/src/test/java/com/website/rajtharan/RajtharanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.rajtharan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajtharanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
